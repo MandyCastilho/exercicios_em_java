02a7bef3bc7ea6078885b7ed9d6f99c5f96df6b7
Amanda Castilho <165108783+MandyCastilho@users.noreply.github.com>
2025-12-08 12:12:23 -0300

    Primeiro Exercício
    Iniciei meus estudos em Java com um Hello World e um programa simples exibindo meus dados.

---
+++ MeusDados.java
@@ -0,0 +1,10 @@
+public class MeusDados {
+    public static void main(String[] args) {
+        System.out.println("=== Meus Dados ===");
+        System.out.println("Nome: Amanda");
+        System.out.println("Faculdade: Análise e Desenvolvimento de Sistemas");
+        System.out.println("Semestre: 5º semestre");
+        System.out.println("Habilidades: HTML, CSS, JS, C++, Python, C#, Node, React, PHP");
+        System.out.println("Status do Java: Aprendendo.");
+    }
+}
