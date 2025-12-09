d33fcbceef0e1fcdcdff156d9f86b0bea2b26026
Amanda Castilho <165108783+MandyCastilho@users.noreply.github.com>
2025-12-09 09:29:35 -0300

    Aula 01
    Adicionei um comentário explicando o que aprendi na aula.

--- aula01/HelloEstiloso.java
+++ aula01/HelloEstiloso.java
@@ -5,3 +5,13 @@ public class HelloEstiloso {
         System.out.println("╚══════════════════════════════╝");
     }
 }
+
+/* NESTA AULA EU APRENDI:
+
+O que é Java;
+Como funciona a JVM;
+Estrutura de um programa: main, System.out.println
+
+*/
+
+
