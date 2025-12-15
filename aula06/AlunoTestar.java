f8e45f9cf3b139eb081aabce4d3dd93c1d046f7b
Amanda Castilho <165108783+MandyCastilho@users.noreply.github.com>
2025-12-15 13:05:22 -0300

    Aula 06
    Nesta aula, estudei o uso de getters e setters, construtores e modificadores de acesso (private, public e protected) na Programação Orientada a Objetos em Java.

---
+++ aula06/AlunoTestar.java
@@ -0,0 +1,15 @@
+public class AlunoTestar {
+    public static void main(String[] args) {
+
+        Aluno aluno1 = new Aluno();
+
+        //aluno.1.nome = "Amanda";
+
+        aluno1.setNome("Amanda");
+        System.out.println(aluno1.getNome());
+
+        //aluno.idade = 500;
+
+        aluno1.setIdade(22);
+    }
+}
