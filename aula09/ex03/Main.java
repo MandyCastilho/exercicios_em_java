ca74c4a3d73cc4236a7d1592e8e81b18e12422a3
Amanda Castilho <165108783+MandyCastilho@users.noreply.github.com>
2025-12-18 10:15:01 -0300

    Aula 09
    Criei uma classe utilitária em Java contendo métodos estáticos reutilizáveis. A classe Utilidades fornece funções auxiliares para exibição de mensagens e operações simples, sendo utilizada diretamente pela classe Main sem necessidade de instanciação.

---
+++ aula09/ex03/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Utilidades.mostrarMensagem("Hello, Java!");
+        System.out.println(Utilidades.dobrar(5));
+    }
+}
---
+++ aula09/ex03/Utilidades.java
@@ -0,0 +1,10 @@
+class Utilidades {
+
+    static void mostrarMensagem(String msg) {
+        System.out.println(msg);
+    }
+
+    static int dobrar(int x) {
+        return x * 2;
+    }
+}
