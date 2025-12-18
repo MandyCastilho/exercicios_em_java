d1a043b1946b42656549b5e65ee99dfe033f7740
Amanda Castilho <165108783+MandyCastilho@users.noreply.github.com>
2025-12-18 09:54:53 -0300

    Aula 09
    Implementei um exemplo de uso de classes e métodos estáticos em Java, utilizando o modificador static para acessar o método somar da classe Calculadora sem a necessidade de instanciar objetos. O código demonstra como métodos estáticos pertencem à classe e podem ser chamados diretamente.

---
+++ aula09/ex01/Calculadora.java
@@ -0,0 +1,6 @@
+class Calculadora {
+
+    static int somar(int a, int b) {
+        return a + b;
+    }
+}
---
+++ aula09/ex01/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int resultado = Calculadora.somar(2, 3);
+        System.out.println(resultado);
+    }
+}
